700d8db47afd053170f9281f703cd72b39a1422d
Aryan Kashyap <matrixapp13@gmail.com>
2016-12-17 20:59:18 +0000

    Initial Commit

---
+++ OpenGL-LoadingModel/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpenGL-LoadingModel
+//
+//  Created by Aryan Kashyap on 17/12/2016.
+//  Copyright © 2016 Aryan Kashyap. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
